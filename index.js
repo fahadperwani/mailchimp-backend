ec9b120a5ef28c0f55b57d75d9a41b6a012b3c8e
fahadperwani <fahadperwani786@gmail.com>
2025-03-24 21:18:35 +0500

    Uninstalled dotenv package

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const { addContact, getCampaigns } = require("./mailchimp");
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
