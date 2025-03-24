2b2f4f4c2752633c1005fa2b0674378af70591d3
fahadperwani <fahadperwani786@gmail.com>
2025-03-24 21:16:17 +0500

    Some necessary changes for deployment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const { addContact, getCampaigns } = require("./mailchimp");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Express on Vercel", process.env.MAILCHIMP_API_KEY));
+app.get("/", (req, res) => res.status(200).send(`"Express on Vercel", ${process.env.MAILCHIMP_API_KEY}`));
 
 app.post("/addContact", async (req, res) => {
     try {
